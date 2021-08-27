7fc318331f280abf9d76d2efdadf9eee0dee7edd
mmokhathi <mmokhathi@gmail.com>
2021-08-27 07:52:17 -0700

    alphabet lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - will print the alphabet in small case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 # 0-positive_or_negative.c will assign a random number to the variable n each time it is executed.
 # 1-last_digit.c will print the last didgit of any number
+# 2-print_alphabet.c will print the alphabet in lower case
