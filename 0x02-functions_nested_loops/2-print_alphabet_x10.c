6e7d4f4143c1d4f6281380244deaa796c4c1b175
mmokhathi <mmokhathi@gmail.com>
2021-08-31 08:27:06 -0700

    alphabet * 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10(void) - prints alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	i = 0;
+
+	while (i <= 9){
+		ch = 'a';
+		while (ch <= 'z'){
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 # 0-holberton.c is a program that prints Holberton followed by newline
 # 1-alphabet.c will print lowercase alphabet
+# 2-print_alphabet_x10.c prints 10 times the alphabet, in lowercase, followed by a new line
