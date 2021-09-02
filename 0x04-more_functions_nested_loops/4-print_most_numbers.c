fdd80e3c1017db3b58ca5bc9ac57a78cd93d24ca
mmokhathi <mmokhathi@gmail.com>
2021-09-02 11:41:32 -0700

    all digits but 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints decimal numbers except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 # 1-isdigit.c checks for a digit
 # 2-mul.c multiplies two integers
 # 3-print_numbers.c prints the numbers 0 to 9
+# 4-print_most_numbers.c prints numbers from 0-9 except 2 and 4
