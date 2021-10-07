32aab3c186f0e4e1a60bfe4ca2c6b2d8354da733
mmokhathi <mmokhathi@gmail.com>
2021-10-07 10:02:50 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * the pointers point to 98 and 42
+ * @a: pointer to the variable swap
+ * @b: pointer to the variable to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
